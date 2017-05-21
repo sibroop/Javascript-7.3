d31d967b78727f08f35094d9cf14e0ea0fcea7a1
sibroop <sibrooptalukdar.st@gmail.com>
2017-05-21 21:00:10 +0530

    Javascript 7.3

---
+++ myjs.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+function myFunction(){
+	var myNumber = getElementById("myNumber").value;
+	var getNum;
+	getNum = myNumber;
+	document.getElementById("demo").innerHTML = myNumber;
+}
